3200fcb8a6e601e6701c40fc86b2b9dbc17c8771
peibolcoru <pabloferreno.@gmail.com>
2024-03-10 19:28:13 +0100

    actualizar inports de iconos

--- src/components/Audiovisual/FooterAudiovisual/FooterAudiovisual.jsx
+++ src/components/Audiovisual/FooterAudiovisual/FooterAudiovisual.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React from 'react';
 import './FooterAudiovisual.css';
-export const FooterAudiovisual = ({contenido}) => {
-    console.log(contenido.programas)
+
+import premiere from '../../../assets/iconos/audiovisuales/adobe-premiere.svg';
+import aftereffects from '../../../assets/iconos/audiovisuales/after-effects.svg';
+import finalcut from '../../../assets/iconos/audiovisuales/final-cut.svg';
+import photoshop from '../../../assets/iconos/audiovisuales/photoshop.svg';
+
+export const FooterAudiovisual = ({ contenido }) => {
+  const tecnologias = {
+    premiere,
+    aftereffects,
+    finalcut,
+    photoshop,
+  };
+  
   return (
     <div className='footerAudiovisual-container'>
       <p>tecnologías:</p>
       <div className='audiovisual-iconos-container'>
-        {contenido.programas.map((programa)=>(
-            <img src={`../../../../public/iconos/audiovisuales/${programa}.svg`} alt="" />
+        {contenido.programas.map((programa) => (
+          <img src={tecnologias[programa]} alt='' />
         ))}
       </div>
-        
     </div>
-  )
-}
+  );
+};
--- src/components/MainAudiovisualTecnoPages/MainAudiovisualTecnoPages.jsx
+++ src/components/MainAudiovisualTecnoPages/MainAudiovisualTecnoPages.jsx
@@ -1,32 +1,45 @@
 import React from 'react';
 import './MainAudiovisualTecnoPages.css';
+
+import premiere from '../../assets/iconos/audiovisuales/adobe-premiere.svg'
+import aftereffects from '../../assets/iconos/audiovisuales/after-effects.svg'
+import finalcut from '../../assets/iconos/audiovisuales/final-cut.svg'
+import photoshop from '../../assets/iconos/audiovisuales/photoshop.svg'
+
 export const MainAudiovisualTecnoPages = () => {
+  const tecnologias = {
+    premiere,
+    aftereffects,
+    finalcut,
+    photoshop,
+  };
+  
   return (
     <div className='mainAudioVisualTecnoPages-container'>
       <div className='audiovisualTecno-container'>
         <img
-          src='../../../public/iconos/audiovisuales/final-cut.svg'
+          src={tecnologias.finalcut}
           alt='final-cut'
         />
         <h3>Final Cut Pro</h3>
       </div>
       <div className='audiovisualTecno-container'>
         <img
-          src='../../../public/iconos/audiovisuales/adobe-premiere.svg'
+          src={tecnologias.premiere}
           alt='final-cut'
         />
         <h3>Adobe Premiere</h3>
       </div>
       <div className='audiovisualTecno-container'>
         <img
-          src='../../../public/iconos/audiovisuales/after-effects.svg'
+          src={tecnologias.aftereffects}
           alt='final-cut'
         />
         <h3>After Effects</h3>
       </div>
       <div className='audiovisualTecno-container'>
         <img
-          src='../../../public/iconos/audiovisuales/photoshop.svg'
+          src={tecnologias.photoshop}
           alt='final-cut'
         />
         <h3>Photoshop</h3>
