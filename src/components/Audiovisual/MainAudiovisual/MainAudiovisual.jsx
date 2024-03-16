22429397eba665db73dd623c76a0e7a56b4558fe
peibolcoru <pabloferreno.@gmail.com>
2024-03-16 10:26:24 +0100

    iframe fullscreen

--- src/components/Audiovisual/MainAudiovisual/MainAudiovisual.jsx
+++ src/components/Audiovisual/MainAudiovisual/MainAudiovisual.jsx
@@ -15,7 +15,7 @@ export const MainAudiovisual = ({ contenido }) => {
             src={`https://www.youtube.com/embed/${contenido.video_id_youtube}`}
             title='YouTube video player'
             frameborder='0'
-            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; fullscreen'
             allowfullscreen
           ></iframe>
     </div>
