53d0f25ca421af0d8154925c179efdc893b5301d
peibolcoru <pabloferreno.@gmail.com>
2024-03-18 21:28:50 +0100

    link curriculum

--- src/components/FooterPpal/FooterPpal.jsx
+++ src/components/FooterPpal/FooterPpal.jsx
@@ -5,25 +5,38 @@ import youtubeIcon from '../../assets/iconos/icons8-youtube2.svg';
 import linkedinIcon from '../../assets/iconos/icons8-linkedin.svg';
 import githubIcon from '../../assets/iconos/icons-github.svg';
 export const FooterPpal = () => {
-  
   return (
-    
     <footer className='ppalFooter-container'>
       <div className='enlaces-general-container'>
-        <a href='https://www.linkedin.com/in/pablo-ferreno-veiga/' target='_blank'
-              rel='noopener noreferrer'>
+        <a
+          href='https://www.linkedin.com/in/pablo-ferreno-veiga/'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
           <img src={linkedinIcon} alt='linkedin' />
         </a>
-        <a href='https://github.com/peibolcoru' target='_blank'
-              rel='noopener noreferrer'>
+        <a
+          href='https://github.com/peibolcoru'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
           <img src={githubIcon} alt='github' />
         </a>
-        <a href='https://www.youtube.com/channel/UCayLZMzKTYX-B-qDRldXhRg' target='_blank'
-              rel='noopener noreferrer'>
+        <a
+          href='https://www.youtube.com/channel/UCayLZMzKTYX-B-qDRldXhRg'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
           <img src={youtubeIcon} alt='youtube' />
         </a>
       </div>
-      <a  href='https://drive.google.com/file/d/1XVa4ArfB19PfSl-1yXhNqX9R9MuDwxGu/view?usp=drivesdk'>curriculum</a>
+      <a className='curriculum'
+        href='https://drive.google.com/file/d/1XVa4ArfB19PfSl-1yXhNqX9R9MuDwxGu/view?usp=drivesdk'
+        target='_blank'
+        rel='noopener noreferrer'
+      >
+        curriculum
+      </a>
     </footer>
   );
 };
