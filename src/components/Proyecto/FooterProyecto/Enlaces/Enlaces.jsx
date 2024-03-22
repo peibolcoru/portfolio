d0948efd2d6bf2f3ee35d8400c3c5e9f03836e64
peibolcoru <pabloferreno.@gmail.com>
2024-03-22 20:53:05 +0100

    solucion links vacios

--- src/components/Proyecto/FooterProyecto/Enlaces/Enlaces.jsx
+++ src/components/Proyecto/FooterProyecto/Enlaces/Enlaces.jsx
@@ -4,15 +4,32 @@ import { v4 as uuidv4 } from 'uuid';
 
 import gitIcon from '../../../../assets/iconos/icons-github.svg';
 import youtubeIcon from '../../../../assets/iconos/icons8-youtube.svg';
+import { useEffect, useState } from 'react';
 
 
 export const Enlaces = ({proyecto}) => {
+  const [displayYoutube,setDisplayYoutube]=useState("");
+  const [displayEnlaces,setDisplayEnlaces]=useState("")
+  useEffect(() => {
+    if (proyecto.Github.length<1  && proyecto.Youtube_link === null) {
+      setDisplayEnlaces("apagado");
+    } else {
+      setDisplayEnlaces("");
+    }
+
+    if (proyecto.Youtube_link === null) {
+      setDisplayYoutube("apagado");
+    } else {
+      setDisplayYoutube("");
+    }
+  }, [proyecto.Github , proyecto.Youtube_link]);
+
   return (
-    <div className='links-proyecto'>
+    <div className={`links-proyecto ${displayEnlaces}`}>
         <p>enlaces</p>
         <div className='icons-container'>
           <div className='proyecto-gitHubLinks'>
-            {proyecto.Github.map((item) => (
+            {proyecto.Github.map((item) =>(
               <a
                 key={uuidv4()}
                 href={item}
@@ -23,7 +40,7 @@ export const Enlaces = ({proyecto}) => {
               </a>
             ))}
           </div>
-          <div className='youtubeLink'>
+          <div className={`youtubeLink ${displayYoutube}`}>
             <a
               href={proyecto.Youtube_link}
               target='_blank'
